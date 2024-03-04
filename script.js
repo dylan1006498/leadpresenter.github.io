9fa041e8b11b559fae9a1b9185a960f5e91837a8
dylan1006498 <54187375+dylan1006498@users.noreply.github.com>
2024-03-04 16:58:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const loginForm = document.getElementById('login-form');
+  const portalContainer = document.querySelector('.portal-container');
 
+  loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    // Check username and password
+    // For simplicity, let's assume the login is successful
+    // Replace this with actual authentication logic
+    if (username === 'dk1006498' && password === 'Washu0112!') {
+      loginForm.reset();
+      document.querySelector('#user').textContent = "Dylan Karpf";
+      loginForm.style.display = 'none';
+      portalContainer.style.display = 'block';
+    } else {
+      alert('Invalid username or password');
+    }
+  });
+});
